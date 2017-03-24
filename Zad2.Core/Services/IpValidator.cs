082ece65736a5939b8e6145a7c0f0dfdbdbcdb0c
severiusuz <severiusuz@gmail.com>
2017-03-24 14:30:03 +0100

    Tak 127.0.0.1 jest poprawnym ip

---
+++ Zad2.Core/Services/IpValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Zad2.Core.Services
+{
+    public class IpValidator : IIpValidator
+    {
+        private const string Pattern = @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b";
+
+        public bool Validate(string ip)
+        {
+            var regex = new Regex(Pattern);
+            return regex.IsMatch(ip);
+        }
+    }
+}
---
+++ Zad2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zad2.Core.Services;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IIpValidator validator = new IpValidator();
+            
+            Console.WriteLine("Podaj adres IP");
+            var input = Console.ReadLine();
+
+            if (validator.Validate(input))
+            {
+                Console.WriteLine("Poprawny");
+            }
+            else
+            {
+                Console.WriteLine("Bledny");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
